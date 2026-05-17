7d0f82f05a6966fdea7be0766736a32c04b1c2dc
Jumdax <brittdagray@gmail.com>
2026-05-17 18:42:46 -0500

    Initial commit

---
+++ Assets/Scripts/Actions/SpinAction.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinAction : BaseAction
+{
+    private float totalSpinAmount;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isActive)
+        {
+            return;
+        }
+        float spinAmount = 360f * Time.deltaTime;
+        transform.eulerAngles += new Vector3(0, spinAmount, 0);
+
+        totalSpinAmount += spinAmount;
+        if(totalSpinAmount >= 360f)
+        {
+            isActive = false;
+            onActionComplete();
+        }
+    }
+
+    public override void TakeAction(GridPosition gridPosition, Action onActionComplete)
+    {
+        this.onActionComplete = onActionComplete;
+        isActive = true;
+        totalSpinAmount = 0f;
+    }
+
+    public override string GetActionName()
+    {
+        return "Spin";
+    }
+    public override List<GridPosition> GetValidActionGridPositionList()
+    {
+        List<GridPosition> validGridPositionList = new List<GridPosition>();
+        GridPosition unitGridPosition = unit.GetGridPosition();
+
+        return new List<GridPosition>
+        {
+            unitGridPosition
+        };
+    }
+
+    public override int GetActionPointCost()
+    {
+        return 2;
+    }
+}
---
+++ Assets/Scripts/Testing.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Testing : MonoBehaviour
+{
+    [SerializeField] private Unit unit;
+
+    private void Start()
+    {
+        Debug.Log("Testing.cs");
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            GridSystemVisual.Instance.HideAllGridPosition();
+            GridSystemVisual.Instance.ShowGridPositionList(
+                unit.GetMoveAction().GetValidActionGridPositionList());   
+        }
+    }
+}
