818cc19d5fdccb0e0ff22f4476e04aa06e48879b
Jumdax <brittdagray@gmail.com>
2026-05-28 23:26:50 -0500

    RagdollExplosion

--- Assets/Scripts/Actions/ShootAction.cs
+++ Assets/Scripts/Actions/ShootAction.cs
@@ -5,6 +5,13 @@ using UnityEngine;
 
 public class ShootAction : BaseAction
 {
+    public event EventHandler<OnShootEventArgs> OnShoot;
+
+    public class OnShootEventArgs : EventArgs
+    {
+        public Unit targetUnit;
+        public Unit shootingUnit;
+    }
     private enum State
     {
         Aiming,
@@ -73,7 +80,13 @@ public class ShootAction : BaseAction
 
     private void Shoot()
     {
-        targetUnit.Damage();
+        OnShoot?.Invoke(this, new OnShootEventArgs
+        {
+            targetUnit = targetUnit,
+            shootingUnit = unit
+        });
+        
+        targetUnit.Damage(40);
     }
     public override string GetActionName()
     {
