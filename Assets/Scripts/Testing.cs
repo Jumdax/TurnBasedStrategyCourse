4ba2af0861f14a117157d30e203504298046c9f5
Jumdax <brittdagray@gmail.com>
2026-06-23 13:34:32 -0500

    Updated Screen Shake

--- Assets/Scripts/Testing.cs
+++ Assets/Scripts/Testing.cs
@@ -16,20 +16,28 @@ public class Testing : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.T))
         {
-            GridPosition mouseGridPosition = LevelGrid.Instance.GetGridPosition(MouseWorld.GetPosition());
-            GridPosition startGridPosition = new GridPosition(0, 0);
 
-            List<GridPosition> gridPositionList = Pathfinding.Instance.FindPath(startGridPosition, mouseGridPosition, out int pathLength);
+            
+            // --- ScreenShake Test ---
+            // ScreenShake.Instance.Shake(5f);
 
-            for (int i = 0; i < gridPositionList.Count - 1; i++)
-            {
-                Debug.DrawLine(
-                    LevelGrid.Instance.GetWorldPosition(gridPositionList[i]),
-                    LevelGrid.Instance.GetWorldPosition(gridPositionList[i + 1]),
-                    Color.white,
-                    10f
-                );
-            }
+            
+            // --- Pathfinding Test Start    
+            // GridPosition mouseGridPosition = LevelGrid.Instance.GetGridPosition(MouseWorld.GetPosition());
+            // GridPosition startGridPosition = new GridPosition(0, 0);
+
+            // List<GridPosition> gridPositionList = Pathfinding.Instance.FindPath(startGridPosition, mouseGridPosition, out int pathLength);
+
+            // for (int i = 0; i < gridPositionList.Count - 1; i++)
+            // {
+            //     Debug.DrawLine(
+            //         LevelGrid.Instance.GetWorldPosition(gridPositionList[i]),
+            //         LevelGrid.Instance.GetWorldPosition(gridPositionList[i + 1]),
+            //         Color.white,
+            //         10f
+            //     );
+            // }
+            // --- Pathfinding Test End
         }
     }
 
